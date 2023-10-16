3803bf13dcd7eeae5983e955a5cbcc4d3d4c319d
shaika-hassan <shaikahassan23@gmail.com>
2023-10-16 13:24:50 +0530

    using first existing core service from the container -logger.factory into the created service dino.roar to diplay it in recent logs

--- modules/custom/dino_roar/src/Controller/RoarController.php
+++ modules/custom/dino_roar/src/Controller/RoarController.php
@@ -3,25 +3,33 @@
 namespace Drupal\dino_roar\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\dino_roar\Service\RoarGenerator;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoarController extends ControllerBase {
   protected $roarGenerator;
+  protected $loggerFactory;
 
-  public function __construct(RoarGenerator $roarGenerator) {
+  public function __construct(RoarGenerator $roarGenerator, LoggerChannelFactoryInterface $loggerFactory) {
     $this->roarGenerator = $roarGenerator;
+    $this->loggerFactory = $loggerFactory;
   }
 
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container)
+  {
     return new static(
-      $container->get('dino_roar.roar_generator')
+      $container->get('dino_roar.roar_generator'),
+      $container->get('logger.factory')
     );
   }
 
   public function roar($count = 5) {
     $roar = $this->roarGenerator->getRoar($count);
+    $this->loggerFactory->get('default')
+         ->debug($roar);
+
     return new Response($roar);
   }
 }
---
+++ sites/default/files/php/twig/652cea4f1cf47_views-view-field.html.twi_73r_fMHGy9kgFt7MKbHvR7-fB/8ddhKEJ-jxypzamlw12-hCYdVwlOvM9QFmc0g13h8gg.php
@@ -0,0 +1,95 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/modules/views/templates/views-view-field.html.twig */
+class __TwigTemplate_9a6bb40eef60ee5bb3cf815805d16c37 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520 = $this->extensions["Drupal\\tracer\\Twig\\Extension\\TraceableProfilerExtension"];
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->enter($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "core/modules/views/templates/views-view-field.html.twig"));
+
+        // line 23
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["output"] ?? null), 23, $this->source), "html", null, true);
+        
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->leave($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/views/templates/views-view-field.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  42 => 23,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/modules/views/templates/views-view-field.html.twig", "C:\\xampp\\htdocs\\Activity\\core\\modules\\views\\templates\\views-view-field.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 23);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
